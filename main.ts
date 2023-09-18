8bb433914b2458c68ab6dbc8070091ad59b5b75c
JossueBarcia <145305148+JossueBarcia@users.noreply.github.com>
2023-09-17 20:26:23 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 class ConsoleUI {
     public static mostrarInterfaz(): void {
-      console.clear();
+      console.clear();// Nombre de la Empresa que lleva el control.
       console.log("========================================");
       console.log("           Aplicación de Inventario      ");
       console.log("========================================\n");
-  
+  // Son los diferentes tipos de interraciones.
       console.log("Opciones:");
       console.log("1. Agregar Producto");
       console.log("2. Eliminar Producto");
       console.log("3. Listar Productos");
       console.log("4. Salir\n");
     }
-  
+  //Mostramos el menesaje.
     public static mostrarMensaje(mensaje: string): void {
       console.log(`\n${mensaje}\n`);
     }
