2ec39e89701887cee52bcd7a31dceff79708fe1e
Artiles Enriquez Marcos Javier <e0961905908@live.uleam.edu.ec>
2023-09-20 15:34:37 -0500

    actualizacion de la clase

--- interfaz.ts
+++ interfaz.ts
@@ -1,13 +1,13 @@
 class ConsoleUI {
     public static mostrarInterfaz(): void {
-      console.clear();// Nombre de la Empresa que lleva el control.
+      console.clear();
       console.log("========================================");
       console.log("           Aplicación de Inventario      ");
       console.log("========================================\n");
-  // Son los diferentes tipos de interraciones.
+  // Son los diferentes tipos de interacciones.
       console.log("Opciones:");
       console.log("1. Agregar Producto");
-     console.log("2. Eliminar Producto");
+      console.log("2. Eliminar Producto");
       console.log("3. Listar Productos");
       console.log("4. Salir\n");
     }
@@ -16,7 +16,6 @@ class ConsoleUI {
       console.log(`\n${mensaje}\n`);
     }
   }
-  // Hola MUNDO 
-  // Ejemplo de uso
+
   ConsoleUI.mostrarInterfaz();
   ConsoleUI.mostrarMensaje("¡Bienvenido a la aplicación de inventario!");
