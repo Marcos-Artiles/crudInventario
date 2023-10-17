f11f85d287819c3231960b33afac0fcb74396bf7
JossueBarcia <145305148+JossueBarcia@users.noreply.github.com>
2023-10-16 23:15:46 -0500

    Update 2.0 bd.ts

--- bd.ts
+++ bd.ts
@@ -29,3 +29,39 @@ export class Producto {
     }
   }
 }
+function ingresarProducto(): Producto {
+
+  const prompt = promptSync();
+  const nombre = prompt("Ingrese el nombre del producto: ") || "";
+  const codigo = parseInt(prompt("Ingrese el código del producto: ") || "0");
+  const precio = parseFloat(prompt("Ingrese el precio del producto: ") || "0");
+
+  return new Producto(nombre, codigo, precio);
+}
+
+function main() {
+
+  const prompt = promptSync();
+  const productosAlmacenados: Producto[] = [];
+
+  const numProductos = parseInt(prompt("\nIngrese el número de productos que desea ingresar: ") || "0");
+
+  for (let i = 1; i <= numProductos; i++) {
+
+    console.log(`\nIngrese los datos para el producto #${i}`);
+    const nuevoProducto = ingresarProducto();
+    productosAlmacenados.push(nuevoProducto);
+    
+  }
+
+  console.log("\nProductos almacenados:");
+  Producto.mostrarProductos(productosAlmacenados);
+
+  const codigoEliminar = parseInt(prompt("Ingrese el código del producto que desea eliminar: ") || "0");
+  Producto.eliminarProductoPorCodigo(productosAlmacenados, codigoEliminar);
+
+  console.log("\nProductos almacenados después de eliminar:");
+  Producto.mostrarProductos(productosAlmacenados);
+}
+
+main();
