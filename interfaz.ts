b1a1bbf4b4e305cdad281a272b966d5a503a714c
CasanovaBryan <145305541+CasanovaBryan@users.noreply.github.com>
2023-10-16 23:11:18 -0500

    Update interfaz.ts

--- interfaz.ts
+++ interfaz.ts
@@ -1,15 +1,14 @@
 export class ConsoleUI {
 
-    public static mostrarInterfaz(): void {
-      console.clear();
-      console.log("¡Bienvenido a la aplicación de inventario!\n");
-      console.log("========================================");
-      console.log("           Aplicación de Inventario      ");
-      console.log("========================================\n");
-      console.log("1. Agregar Producto");
-      console.log("2. Eliminar Producto");
-      console.log("3. Listar Productos");
-      console.log("4. Salir\n");
-
-    }
+  public static mostrarInterfaz(): void {
+    console.clear();
+    console.log("¡Bienvenido a la aplicación de inventario!\n");
+    console.log("========================================");
+    console.log("           Aplicación de Inventario      ");
+    console.log("========================================\n");
+    console.log("1. Agregar Producto");
+    console.log("2. Eliminar Producto");
+    console.log("3. Salir\n");
   }
+
+}
